c590d3061124d7bfbdb929e5de1580b6ebc55c34
Jeff Michaud <cometaj2@comcast.net>
2019-06-02 19:52:57 -0700

    basic dict in dict support

--- hcli_core/haliot/hal.py
+++ hcli_core/haliot/hal.py
@@ -4,14 +4,15 @@ class Resource:
     _links = None
 
     def __init__(self, model):
-        self._links = []
+        self._links = {}
         for key, value in vars(model).items():
             setattr(self, key, value)
 
     def addLink(self, link):
         if link != None:
-            self._links.append(link)
-            #self._links.update(link)
+            for key, value in vars(link).items():
+                l = dict(key=key)
+                self._links.update(self=value)
 
     def toHALJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
