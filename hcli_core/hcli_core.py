6fcab3d839295bcfcc94001693e918e2c684c9c1
Jeff Michaud <cometaj2@comcast.net>
2019-06-07 19:58:12 -0700

    call the class instead of instantiating controllers

--- hcli_core/hcli_core.py
+++ hcli_core/hcli_core.py
@@ -11,9 +11,9 @@ from hcli import execution
 from hcli import finalexecution
 
 server = falcon.API()
-server.add_route(home.HomeController().route, api.HomeApi())
-server.add_route(document.DocumentController().route, api.DocumentApi())
-server.add_route(command.CommandController().route, api.CommandApi())
-server.add_route(option.OptionController().route, api.OptionApi())
-server.add_route(execution.ExecutionController().route, api.ExecutionApi())
-server.add_route(finalexecution.FinalExecutionController().route, api.FinalExecutionApi())
+server.add_route(home.HomeController.route, api.HomeApi())
+server.add_route(document.DocumentController.route, api.DocumentApi())
+server.add_route(command.CommandController.route, api.CommandApi())
+server.add_route(option.OptionController.route, api.OptionApi())
+server.add_route(execution.ExecutionController.route, api.ExecutionApi())
+server.add_route(finalexecution.FinalExecutionController.route, api.FinalExecutionApi())
