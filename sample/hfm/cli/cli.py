c94a601d9f1a035ca268345de5248c8db6d6ecd9
Jeff Michaud <cometaj2@comcast.net>
2019-06-15 14:44:51 -0700

    save cli code for hfm in samples

--- sample/hfm/cli/cli.py
+++ sample/hfm/cli/cli.py
@@ -10,20 +10,71 @@ class CLI:
         self.inputstream = inputstream
 
     def execute(self):
-        print(self.commands)
 
-        if self.commands[1] == "--version":
-            return io.BytesIO(b"1.0.1\n")
-
-        if self.commands[1] == "go":
-            return self.pretty(self.inputstream)
-
-        return None
-
-    def pretty(self, inputstream):
         if self.inputstream != None:
-            string = json.loads(self.inputstream.decode("utf-8"))
-            j = json.dumps(string, indent=4) + "\n"
-            return io.BytesIO(j.encode("utf-8"))
+            print(self.commands)
+            self.upload()
+            return None
 
-        return None
+        elif self.inputstream == None:
+            return self.download()
+
+    def upload(self):
+        None
+
+    def download(self):
+        None
+
+#    public InputStream execute(String[] commands, InputStream is) throws Exception
+#    {
+#        if(is != null)
+#        {
+#            upload(is);
+#            return null;
+#        }
+#        else
+#        {
+#            return download();
+#        }
+#    }
+
+#    private void upload(InputStream is) throws FileNotFoundException
+#    {
+#        if (is != null)
+#        {
+#            File f = new File("/Users/jeff/Desktop/test.mov");
+#            FileOutputStream fos = new FileOutputStream(f);
+#"CLI.java" 70L, 1752C
+#            {
+#                int bufferSize = 1048576;
+#                byte[] b = new byte[bufferSize];
+#                while ((bufferSize = is.read(b)) != -1)
+#                {
+#                   fos.write(b, 0, bufferSize);
+#                   fos.flush();
+#                }
+#
+#                is.close();
+#                fos.close();
+#            }
+#            catch (IOException e)
+#            {
+#                e.printStackTrace();
+#            }
+#            finally
+#            {
+#                IOUtils.closeQuietly(is);
+#                IOUtils.closeQuietly(fos);
+#            }
+#        }
+#
+#        return;
+#    }
+#
+#    public InputStream download() throws FileNotFoundException
+#    {
+#        File attachment = new File("/Users/jeff/Desktop/DontStarve.mov");
+#        final FileInputStream fis = new FileInputStream(attachment);
+#
+#        return fis;
+#    }    
