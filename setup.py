df751538b65ffe2985dfb94d6681212679d85187
Jeff Michaud <cometaj2@comcast.net>
2022-04-24 17:23:43 -0700

    add configuration to generate a wheel for pypi publishing

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ if sys.argv[-1] == 'publish':
         sys.exit("publishing from a branch other than master is disallowed.")
     os.system("rm -rf dist")
     os.system("python setup.py sdist")
+    os.system("python setup.py bdist_wheel")
     os.system("twine upload dist/* -r pypi")
     os.system("git tag -a %s -m 'version %s'" % ("hcli_core-" + package.__version__, "hcli_core-" + package.__version__))
     os.system("git push")
