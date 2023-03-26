ca413dd87eeabee5a36808401de1155062f509fb
Jeff Michaud <cometaj2@comcast.net>
2023-03-26 16:37:21 -0700

    attempting to exclude openai and tiktoken dependencies for python version < 3.8

--- hcli_core/package.py
+++ hcli_core/package.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import, division, print_function
 
 __version__ = "0.11.0"
 dependencies = ["falcon==3.1.0",
-                "openai==0.27.2",
-                "tiktoken==0.3.2"]
+                "openai==0.27.2"; python_version >= "3.8",
+                "tiktoken==0.3.2"; python_version >= "3.8"]
