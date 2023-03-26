e4af70debbd118265679dd8cb23ee1295231d4fa
Jeff Michaud <cometaj2@comcast.net>
2023-03-26 16:43:16 -0700

    cut support for python 3.5, 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10'
