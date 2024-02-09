134348aa28854837a7667c0142166aae574948cc
Jeff Michaud <cometaj2@comcast.net>
2024-02-09 13:39:51 -0800

    add python 3.12 tests and library version update.

--- hcli_core/package.py
+++ hcli_core/package.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.19.0"
-dependencies = ["falcon==3.1.1"]
+__version__ = "0.19.1"
+dependencies = ["falcon==3.1.3"]
