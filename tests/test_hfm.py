e99c130444d507112817de9a761ddece9a61d19a
Jeff Michaud <cometaj2@comcast.net>
2024-04-07 19:38:22 -0700

    update tests to the module connector call

--- tests/test_hfm.py
+++ tests/test_hfm.py
@@ -8,7 +8,7 @@ def test_function():
     #!/bin/bash
     set -x
 
-    gunicorn --workers=1 --threads=1 "hcli_core.hcli_core:connector(\\"`hcli_core sample hfm`\\")" --daemon
+    gunicorn --workers=1 --threads=1 "hcli_core:connector(\\"`hcli_core sample hfm`\\")" --daemon
     huckle cli install http://127.0.0.1:8000
     """
 
