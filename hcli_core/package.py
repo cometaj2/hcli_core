13ebb74db79985475f1afc3d5d586f5453693ee0
Jeff Michaud <cometaj2@comcast.net>
2025-01-05 15:39:32 -0800

    update to latest version of falcon and huckle

--- hcli_core/package.py
+++ hcli_core/package.py
@@ -1,4 +1,4 @@
-__version__ = "3.1.1"
-dependencies = ["falcon==4.0.1",
+__version__ = "3.2.0"
+dependencies = ["falcon==4.0.2",
                 "portalocker==2.10.1",
-                "huckle==5.2.1"]
+                "huckle==5.3.0"]
