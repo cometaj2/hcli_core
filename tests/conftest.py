285062f5bcb0748771707251cf8fbc0d587cc416
Jeff Michaud <cometaj2@comcast.net>
2025-03-21 15:34:03 -0700

    add additional test output to troubleshoot circleci

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,14 @@ salt = *" > ./test_credentials
 
     gunicorn --workers=1 --threads=100 -b 0.0.0.0:18000 -b 0.0.0.0:19000 "hcli_core:connector(config_path=\\\"./test_credentials\\\")" --daemon --log-file=./gunicorn.log --error-logfile=./gunicorn-error.log --capture-output
 
-    sleep 4
+    sleep 2
+
+    echo "Checking port status..."
+    netstat -tuln | grep 18000 || echo "Port 18000 not listening"
+    netstat -tuln | grep 19000 || echo "Port 19000 not listening"
+    ps aux | grep '[g]unicorn' || echo "No gunicorn processes found"
+    cat ./gunicorn-error.log
+    cat ./gunicorn.log
 
     grep "Password:" ./gunicorn-error.log | awk '{print $8}' > ./password
     huckle cli install http://127.0.0.1:18000
@@ -53,8 +60,14 @@ salt = *" > ./test_credentials
     huckle cli config jsonf auth.mode basic
 
     """
-    process = subprocess.Popen(['bash', '-c', setup], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process = subprocess.Popen(['bash', '-c', setup], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
+
+    result = out.decode('utf-8')
+    error = err.decode('utf-8')
+
+    print(result)
+    print(error)
 
     # Verify setup worked
     assert os.path.exists('./gunicorn-error.log'), "gunicorn-error.log not found"
