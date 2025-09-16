06415682693729ae63d39b5a2dd3d1fb7fde8004
Jeff Michaud <cometaj2@proton.me>
2025-09-15 17:58:28 -0700

    setup better singleton logic, log output, and rollup to the root logger

--- hcli_core/logger.py
+++ hcli_core/logger.py
@@ -1,10 +1,5 @@
 import logging
-import traceback
-import os
-import io
-import datetime
 import threading
-import collections
 from logging.handlers import RotatingFileHandler
 
 from hcli_core import config
@@ -18,33 +13,53 @@ CRITICAL = logging.CRITICAL
 
 class Logger:
     _instances = {}
-    _lock = threading.Lock()
+    _lock = threading.RLock()
+    _original_record_factory = logging.getLogRecordFactory()
     ROOT_LOGGER = "hcli_core"
 
-    def __new__(cls, name=None, *args, **kwargs):
+    @classmethod
+    def _custom_record_factory(cls, *args, **kwargs):
+        record = cls._original_record_factory(*args, **kwargs)
+
+        # Use the last part of the logger name for display
+        record.display_name = record.name.split('.')[-1]
+        return record
+
+    def __new__(cls, name=None, log=None, *args, **kwargs):
         with cls._lock:
             if name not in cls._instances:
                 instance = super(Logger, cls).__new__(cls)
                 cls._instances[name] = instance
+                instance.init(name, log)
             return cls._instances[name]
 
-    def __init__(self, name=None, log=None, max_bytes=10485760, backup_count=5, *args, **kwargs):
+    def init(self, name=None, log=None, max_bytes=10485760, backup_count=5, *args, **kwargs):
         with self._lock:
-            self.name = name or __name__
-            self.instance = logging.getLogger(self.name)
 
-            # Only add handlers if they haven't been added yet
-            if not self.instance.handlers and name == self.ROOT_LOGGER:
+            # Set custom record factory to modify display name
+            logging.setLogRecordFactory(self._custom_record_factory)
+
+            # Create logger as child of ROOT_LOGGER if name is provided
+            self.name = name or self.ROOT_LOGGER
+            if name and name != self.ROOT_LOGGER:
+                self.logger_name = f"{self.ROOT_LOGGER}.{name}"
+            else:
+                self.logger_name = self.ROOT_LOGGER
+            self.instance = logging.getLogger(self.logger_name)
+
+            # Only configure handlers for the root logger if not already configured
+            root_logger = logging.getLogger(self.ROOT_LOGGER)
+            if not root_logger.handlers:
                 date_format = "%Y-%m-%d %H:%M:%S %z"
-                message_format = "[%(asctime)s] [%(levelname)-8s] [%(name)s] [%(filename)13s:%(lineno)-3s] %(message)s"
+                message_format = "[%(asctime)s] [%(levelname)-8s] [%(display_name)s] [%(filename)13s:%(lineno)-3s] %(message)s"
                 formatter = logging.Formatter(fmt=message_format, datefmt=date_format)
 
+                # Stream handler
                 self.streamHandler = logging.StreamHandler()
                 self.streamHandler.setFormatter(formatter)
-                self.instance.addHandler(self.streamHandler)
+                root_logger.addHandler(self.streamHandler)
 
                 # File handler (if log_file is provided)
-                # NullHandler otherwise to prevent any output
                 if log == 'log':
                     log_file = config.log_file_path
                     self.fileHandler = RotatingFileHandler(
@@ -54,16 +69,15 @@ class Logger:
                         encoding='utf-8'
                     )
                     self.fileHandler.setFormatter(formatter)
-                    self.instance.addHandler(self.fileHandler)
+                    root_logger.addHandler(self.fileHandler)
                 else:
-                    self.instance.addHandler(logging.NullHandler())
+                    root_logger.addHandler(logging.NullHandler())
 
-            else:
-                # If handlers exist, get the existing ones
-                self.streamHandler = next((h for h in self.instance.handlers 
-                                        if isinstance(h, logging.StreamHandler)), None)
-                self.fileHandler = next((h for h in self.instance.handlers
-                                if isinstance(h, RotatingFileHandler)), None)
+            # Retrieve handlers from root logger
+            self.streamHandler = next((h for h in root_logger.handlers 
+                                      if isinstance(h, logging.StreamHandler)), None)
+            self.fileHandler = next((h for h in root_logger.handlers
+                                     if isinstance(h, RotatingFileHandler)), None)
 
     def setLevel(self, level):
         self.instance.setLevel(level)
